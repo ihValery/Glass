677c5df45470fc28dd0b5c2fb25ac32026f667ff
ihValery <ihValery@gmail.com>
2022-05-26 15:16:47 +0300

    refactoring

---
+++ Glass/DragGestureView.swift
@@ -0,0 +1,20 @@
+//
+//  DragGestureView.swift
+//  Glass
+//
+//  Created by Валерий Игнатьев on 26.05.22.
+//
+
+import SwiftUI
+
+struct DragGestureView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DragGestureView_Previews: PreviewProvider {
+    static var previews: some View {
+        DragGestureView()
+    }
+}
